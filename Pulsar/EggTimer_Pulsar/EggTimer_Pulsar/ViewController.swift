d75b6fd0c20c72a4bc1b5d859fbceffcc17ddb06
gkals4417 <gkals4417@gmail.com>
2023-02-23 17:13:53 +0900

    Test3

--- Pulsar/EggTimer_Pulsar/EggTimer_Pulsar/ViewController.swift
+++ Pulsar/EggTimer_Pulsar/EggTimer_Pulsar/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("깃허브는 어려워")
+        print("Test2")
     }
 
 
